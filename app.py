4ed283bec7097d1573451dbefaf92af3b75d12e5
Gabriel Lee <86539726+reeeeg@users.noreply.github.com>
2023-10-07 19:11:20 -0700

    Demo Raw Data setup
    added the button mapping for preliminary graphed images

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
+import pandas as pd 
 import matplotlib.pyplot as plt
+import csv
+import os
 
 #HEADER
 st.header('Finding GeoStorms: using DSCOVR')
@@ -20,6 +22,92 @@ model to process and show 99.99% accurate predictions
 '''
 st.subheader('Our Approach:')
 st.markdown(approachPar)
+
+#bgm_____________________________________________________________
+audio_file = open('bgm.weba', 'rb')
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format='audio/weba')
+
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+st.audio(note_la, sample_rate=sample_rate)
+#bgm end __________________________________________________________
+
+#raw data graphing________________________________________
+col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+t16 = False
+t17 = False
+t18 = False
+t19 = False
+t20 = False
+t21 = False
+t20 = False
+t22 = False
+t23 = False
+with col1:
+    if st.button("2016"):
+        t16 = True
+with col2:
+    if st.button("2017"):
+        t17 = True
+with col3:
+    if st.button("2018"):
+        t18 = True
+with col4:
+    if st.button("2019"):
+        t19 = True
+with col5:
+    if st.button("2020"):
+        t20 = True
+with col6:
+    if st.button("2021"):
+        t21 = True
+with col7:
+    if st.button("2022"):
+        t22 = True
+with col8:
+    if st.button("2023"):
+        t23 = True
+
+if t19 == True:
+    image_path = os.path.join("rawGraphs", "graph2019.jpg")
+    st.image(image_path, caption="graph2019.jpg", use_column_width=True)
+else: 
+    if t23 == True:
+        image_path = os.path.join("rawGraphs", "graph2023.jpg")
+        st.image(image_path, caption="graph2023.jpg", use_column_width=True)
+    else:
+        if t16 == True:
+            image_path = os.path.join("rawGraphs", "graph2016.jpg")
+            st.image(image_path, caption="graph2016.jpg", use_column_width=True)
+        else:
+            if t17 == True:
+                image_path = os.path.join("rawGraphs", "graph2017.jpg")
+                st.image(image_path, caption="graph2017.jpg", use_column_width=True)
+            else:
+                if t18 == True:
+                    image_path = os.path.join("rawGraphs", "graph2018.jpg")
+                    st.image(image_path, caption="graph2018.jpg", use_column_width=True)
+                else:
+                    if t20 == True:
+                        image_path = os.path.join("rawGraphs", "graph2020.jpg")
+                        st.image(image_path, caption="graph2020.jpg", use_column_width=True)
+                    else:
+                        if t21 == True:
+                            image_path = os.path.join("rawGraphs", "graph2021.jpg")
+                            st.image(image_path, caption="graph2021.jpg", use_column_width=True)
+                        else:
+                            if t22 == True:
+                                image_path = os.path.join("rawGraphs", "graph2022.jpg")
+                                st.image(image_path, caption="graph2022.jpg", use_column_width=True)
+#raw data graphing end_____________________________________________________________________________
 
 #DEMO
 demoPar = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
